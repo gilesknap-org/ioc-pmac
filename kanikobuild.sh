0f2c1997b44f2434499f6e957bf594838a9b8901
giles <gilesknap@gmail.com>
2021-03-25 08:55:25 +0000

    test commit

--- kanikobuild.sh
+++ kanikobuild.sh
@@ -3,8 +3,8 @@
 
 # this script is designed for projects below the namespace <group name>/containers
 # and deploys images to:
-#     $PROJECT_PATH in the work registry for untagged commits
-#     $PROJECT_PATH in the prod registry for tagged commits
+#      $PROJECT_PATH in the work registry for untagged commits
+#      $PROJECT_PATH in the prod registry for tagged commits
 # requires that CI_WORK_REGISTRY and CI_PROD_REGISTRY are set to a root path
 # in your image registry for each of work and production images.
 # See Settings->CI->Variables in the gitlab group <group name>/containers
